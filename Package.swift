174e0d739ee705434cb032cd72c3bcb5073317dc
Jason Rodriguez <jrodriguez@simplenexus.com>
2021-02-21 22:29:18 -0700

    Fixes package.swift

--- Package.swift
+++ Package.swift
@@ -5,18 +5,12 @@ let package = Package(
     name: "Lottie",
     platforms: [.iOS(.v9), .macOS("10.10")],
     products: [
-        .library(name: "Lottie", targets: ["Lottie", "LottieMacOS"])
+        .library(name: "Lottie", targets: ["Lottie"])
     ],
     targets: [
         .target(
             name: "Lottie",
-            path: "lottie-swift/src",
-            exclude: ["Public/MacOS"]
-        ),
-        .target(
-            name: "LottieMacOS",
-            path: "lottie-swift/src",
-            exclude: ["Public/iOS"]
+            path: "lottie-swift/src"
         )
     ]
 )
